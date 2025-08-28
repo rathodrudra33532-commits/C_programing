d1f2eff84e5eef23426484f042b7c0f91e9e8e25
rathodrudra33532-commits <rathodrudra33532@gmail.com>
2025-08-29 01:07:04 +0530

    Update 22R.c

--- 22R.c
+++ 22R.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 int main()
 {
-    int a,b;
+    int r,b;
     printf("enter the gross sales:");
-    scanf("%d",&a);
-    b = a - (a*0.1);
+    scanf("%d",&r);
+    b = r - (a*0.1);
     printf("the net sales is :%d",b);
     return 0;
 }
