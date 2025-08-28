908b149e6cac13d1c88582d32ed8ddc91e565ddd
rathodrudra33532-commits <rathodrudra33532@gmail.com>
2025-08-29 00:49:32 +0530

    Update 18R.c

--- 18R.c
+++ 18R.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 int main()
 {
-    int l,area,perimeter;
+    int m,area,perimeter;
     printf("enter length:");
-    scanf("%d",&l);
-    area = l*l;
-    perimeter = 4*l;
+    scanf("%d",&m);
+    area = m*m;
+    perimeter = 4*m;
     printf("area = %d\n perimeter = %d\n", area , perimeter);
     return 0;
 
